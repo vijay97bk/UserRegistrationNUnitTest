ea2afb4761ba37911bb6983348295d4d11911bb1
vijay97bk <75747707+vijay97bk@users.noreply.github.com>
2021-02-12 19:13:26 +0530

    [vijay]firstCommit

---
+++ UserRegistrationNUnitTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UserRegistrationNUnitTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome message");
+        }
+    }
+}
